605354b84ed49d3dd6f7b24e0aba6ba68de642e1
rabyese86 <56589271+rabyese86@users.noreply.github.com>
2022-02-11 20:30:53 +0100

    Add files via upload

---
+++ #Projects/Object Methods.(funtions)/script.js
@@ -0,0 +1,57 @@
+/*object method
+
+var person={
+    firstname: 'john',
+    lastname:   'Andrew'
+
+};
+
+person.greet= function(){
+    console.log('hello')
+
+}
+person.greet()
+
+------------------------------------------------
+
+var person={
+
+    firstname: 'John',
+    lastname: 'andrew'
+};
+
+function greet(){
+
+    console.log('hello world')
+
+}
+
+person.greet=greet;
+person.greet() 
+
+--------------------------------------
+
+var person={
+
+    firstname: 'John',
+    lastname: 'Andrew',
+    greet:function(){
+
+
+        console.log('hello world')
+    }
+};
+person.greet()
+
+var person={
+
+    firstname: 'John',
+    lastname: 'Andrew',
+    greet(){
+
+
+        console.log('hello world')
+    }
+};
+person.greet() */
+
