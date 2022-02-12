f57050cabb8233f8037d941b861bbed135652735
rabyese86 <56589271+rabyese86@users.noreply.github.com>
2022-02-12 19:14:06 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,96 +1,108 @@
-function printError(elemId, hintMsg) {
-    document.getElementById(elemId).innerHTML = hintMsg;
- }
- 
- 
- function validateForm() {
-   
-    var name = document.contactForm.name.value;
-    var email = document.contactForm.email.value;
-    var mobile = document.contactForm.mobile.value;
-    var country = document.contactForm.country.value;
-    var gender = document.contactForm.gender.value;
-    var hobbies = [];
-    var checkboxes = document.getElementsByName("hobbies[]");
-    for(var i=0; i < checkboxes.length; i++) {
-        if(checkboxes[i].checked) {
-          
-            hobbies.push(checkboxes[i].value);
-        }
+/*object method
+
+var person={
+    firstname: 'john',
+    lastname:   'Andrew'
+
+};
+
+person.greet= function(){
+    console.log('hello')
+
+}
+person.greet()
+
+------------------------------------------------
+
+var person={
+
+    firstname: 'John',
+    lastname: 'andrew'
+};
+
+function greet(){
+
+    console.log('hello world')
+
+}
+
+person.greet=greet;
+person.greet() 
+
+--------------------------------------
+
+var person={
+
+    firstname: 'John',
+    lastname: 'Andrew',
+    greet:function(){
+
+
+        console.log('hello world')
     }
-    
- 
-    var nameErr = emailErr = mobileErr = countryErr = genderErr = true;
- 
-    if(name == "") {
-        printError("nameErr", "Please enter your name");
-    } else {
-        var regex = /^[a-zA-Z\s]+$/;                
-        if(regex.test(name) === false) {
-            printError("nameErr", "Please enter a valid name");
-        } else {
-            printError("nameErr", "");
-            nameErr = false;
-        }
+};
+person.greet()
+-----------------------------------------------------------
+var person={
+
+    firstname: 'John',
+    lastname: 'Andrew',
+    greet(){
+
+
+        console.log('hello world')
     }
-    
-    
-    if(email == "") {
-        printError("emailErr", "Please enter your email address");
-    } else {
-        
-        var regex =/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
-        if(regex.test(email) === false) {
-            printError("emailErr", "Please enter a valid email address");
-        } else{
-            printError("emailErr", "");
-            emailErr = false;
-        }
+};
+person.greet() 
+
+------------------------------------------------------------------
+let person ={
+
+    firstname: 'John',
+    lastname: 'Andrew',
+    greet: function(){
+
+        console.log('hello world')
+
+    },
+    getfullname:function(){
+
+        return this.firstname+' '+this.lastname;
     }
-    
-    if(mobile == "") {
-        printError("mobileErr", "Please enter your mobile number");
-    } else {
-        var regex = /^[1-9]\d{9}$/;
-        if(regex.test(mobile) === false) {
-            printError("mobileErr", "Please enter a valid 10 digit mobile number");
-        } else{
-            printError("mobileErr", "");
-            mobileErr = false;
-        }
+};
+
+console.log(person.getfullname()) 
+
+
+------------------------------------------------------------------
+
+
+function People(firstname,lastname){
+
+    this.firstname=firstname
+    this.lastname=lastname
+
+}
+
+let people =new People('John','Andrew')
+
+console.log(people.firstname)  
+----------------------------------------------------------------
+
+const person={
+
+    firstname: 'John',
+    lastname: 'Andrew',
+    Age: 35
+
+};
+
+for(const key in person){
+    if(person.hasOwnProperty(key)){
+
+        const value = person[key];
+
+        console.log(value)
+
     }
-    
-   
-    if(country == "Select") {
-        printError("countryErr", "Please select your country");
-    } else {
-        printError("countryErr", "");
-        countryErr = false;
-    }
-    
- 
-    if(gender == "") {
-        printError("genderErr", "Please select your gender");
-    } else {
-        printError("genderErr", "");
-        genderErr = false;
-    }
-    
-    
-    if((nameErr || emailErr || mobileErr || countryErr || genderErr) == true) {
-       return false;
-    } else {
-     
-        var dataPreview = "You've entered the following details: \n" +
-                          "Full Name: " + name + "\n" +
-                          "Email Address: " + email + "\n" +
-                          "Mobile Number: " + mobile + "\n" +
-                          "Country: " + country + "\n" +
-                          "Gender: " + gender + "\n";
-        if(hobbies.length) {
-            dataPreview += "Hobbies: " + hobbies.join(", ");
-        }
-        
-        alert(dataPreview);
-    }
- };
+}*/
